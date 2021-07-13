6d914529e4b0380736afb108a30f585066030ff6
AK391 <81195143+AK391@users.noreply.github.com>
2021-07-13 19:44:07 -0400

    remove ipd

--- gradiodemo.py
+++ gradiodemo.py
@@ -5,8 +5,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import gin
-import IPython.display as ipd
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
 import torch
